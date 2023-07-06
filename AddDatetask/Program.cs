af0cdc4f211c15a571567e8fb0b420a7a3f7e7a9
Ali <lisalmanov84@gmail.com>
2023-07-06 16:50:05 +0400

    Add Date

---
+++ AddDatetask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace AddDatetask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input date (dd/MM/yyyy): " );
+            string dateTime = Console.ReadLine();
+
+            if (DateTime.TryParseExact(dateTime, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+            {
+                DateTime newDate = date.AddDays(30).AddHours(6).AddMinutes(17);
+                Console.WriteLine("New Date: " + newDate.ToString("dd/MM/yyyy HH:mm"));
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+        }
+    }
+}
